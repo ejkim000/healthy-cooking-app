f9d767038eb8fae9bc35803701232a081808a66c
ejkim000 <developer.ejk@gmail.com>
2023-08-04 22:17:11 -0400

    get foods data from api

--- src/components/NutritionInfo.jsx
+++ src/components/NutritionInfo.jsx
@@ -1,10 +1,13 @@
 
-function NutritionInfo({ data }) {
+function NutritionInfo({ foods }) {
 
-    console.log(data)
+    console.log(foods)
     return (
         <div>
-
+           {foods !== undefined ? (foods.map((food) => {
+            return <h3>{food.description}</h3>
+           })) : null
+        }     
         </div>
     )
 }
--- src/components/SearchIngredient.jsx
+++ src/components/SearchIngredient.jsx
@@ -4,40 +4,49 @@ import { useState } from 'react';
 
 function SearchIngredient() {
 
-    const [nutrition, setNutrition] = useState({});
+    const [foods, setFoods] = useState([]);
     const [ingredient, setIngredient] = useState('');
     const APIKEY = import.meta.env.VITE_API_KEY;
 
-    const url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${ingredient}&api_key=${APIKEY}`;
-    
+    const url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${ingredient}&dataType=SR%20Legacy&api_key=${APIKEY}`;
+
+    const url2 ='https://api.nal.usda.gov/fdc/v1/foods/list?dataType=Branded&pageSize=25&pageNumber=2&api_key=Q4WtkvTm76lg58wT9dtIYG8Iju7anawRNIHyJOTf';
+
 
     const nutritionApiCall = async () => {
         const res = await fetch(url);
         const data = await res.json();
-        console.log(data);
-        setNutrition(data);
+        // console.log(data);
 
+        setFoods(data.foods);
     }
 
-    const handleClick = ({ target }) => {
+    const handleChange = ({ target }) => {
+        // console.log(target.value);
         if (target.value) {
             setIngredient(target.value);
         }
     };
-    
+
     const handleSearch = (e) => {
         e.preventDefault();
-        nutritionApiCall(ingredient);
+        // console.log(ingredient);
+        if (ingredient) {
+            nutritionApiCall(ingredient);
+        } else {
+            console.log('input ingredient name')
+        }
+
     };
 
     return (
         <div>
             <form>
-                <input type="text" name='ingredient' onClick={handleClick} />
+                <input type="text" name='ingredient' placeholder='ingredient' onChange={handleChange} />
                 <button onClick={handleSearch}>Search</button>
             </form>
 
-            <NutritionInfo data={nutrition} />
+            <NutritionInfo foods={foods} />
         </div>
     )
 }
